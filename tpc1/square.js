ee4726d36143334a584c8e75d9e64d4a07f9ffcb
dmgcsilva <43959937+dmgcsilva@users.noreply.github.com>
2018-10-10 12:09:50 +0100

    Zoom formula updated, now use keys Q and E to zoom in and out

--- tpc1/square.js
+++ tpc1/square.js
@@ -6,7 +6,7 @@ window.onload = function init() {
     canvas = document.getElementById("gl-canvas");
     gl = WebGLUtils.setupWebGL(canvas);
     if(!gl) { alert("WebGL isn't available"); }
-    
+
     // Three vertices
     var vertices = [
         vec2(-1,1),
@@ -14,11 +14,11 @@ window.onload = function init() {
         vec2(1,1),
         vec2(1,-1)
     ];
-    
+
     // Configure WebGL
     gl.viewport(0,0,canvas.width, canvas.height);
     gl.clearColor(1.0, 1.0, 1.0, 1.0);
-    
+
     // Load shaders and initialize attribute buffers
     var program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
@@ -32,23 +32,23 @@ window.onload = function init() {
     var vPosition = gl.getAttribLocation(program, "vPosition");
     gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
-    
+
     dragLoc = gl.getUniformLocation(program, "drag");
     gl.uniform3fv(dragLoc, drag);
     scaleLoc = gl.getUniformLocation(program, "scale");
     gl.uniform1f(scaleLoc, scale);
-    
+
     document.body.appendChild(document.createElement("p"));
     createSlide();
-    
+
     slideLoc = gl.getUniformLocation(program, "n");
     gl.uniform1i(slideLoc, slide.value);
-    
+
     createDropDown();
-    
+
     dropDownLoc = gl.getUniformLocation(program, "func");
     gl.uniform1i(dropDownLoc, dropDown.value);
-    
+
     setupCallBacks();
     render();
 }
@@ -77,47 +77,52 @@ function createSlide() {
 function setupCallBacks() {
     canvas.addEventListener("mousedown", function(e) {
         console.log("mousedown");
-        
+
         dragging  = true;
         lastX = e.clientX;
         lastY = e.clientY;
 });
-    
+
     canvas.addEventListener("mouseup", function() {
         console.log("mouseup");
         dragging = false;
 });
-    
+
     canvas.addEventListener("mousemove", function(e) {
         console.log("mousemove");
         if (dragging) {
             var newX = e.clientX,
             newY = e.clientY;
-        
+
             var dx = -1*(newX - lastX),
                 dy = -1*-1*(newY - lastY);
             var model_dx = 2*(dx/canvas.width),
                 model_dy = 2*(dy/canvas.height);
-        
+
             drag[0] += model_dx;
             drag[1] += model_dy;
-        
+
             lastX = newX;
             lastY = newY;
             console.log(drag[0]);
     }
 });
-    canvas.addEventListener("mousewheel", function(e) {
-        console.log("Delta " + e.wheelDelta);
-        var res;
-        zoom += e.wheelDelta;
-        scale = Math.max((canvas.width - zoom)/canvas.width, 0.1);//funciona assumindo que o canvas e quadrado
-        scale = Math.min(scale, 200.0);
-        if(scale == 200.0 || scale == 0.1) {
-            zoom -= e.wheelDelta;
-        }
-        console.log("zoom: " + zoom);
-        console.log("scale: " + scale);
+    window.addEventListener("keypress", function(e) {
+      //console.log("key pressed");
+      switch(e.key) {
+        case 'q':
+          console.log("Q");
+          scale -= 1/500 * scale*2.0;
+          break;
+        case 'e':
+          console.log("E");
+          scale += 1/500 * scale*2.0;
+
+          break;
+        default:
+          console.log("default");
+          break;
+      }
     })
 }
 
@@ -125,11 +130,11 @@ function setupCallBacks() {
 function render() {
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-    
+
     gl.uniform3fv(dragLoc, drag);
     gl.uniform1i(slideLoc, slide.value);
     gl.uniform1i(dropDownLoc, dropDown.value);
     gl.uniform1f(scaleLoc, scale);
-    
+
     requestAnimFrame(render);
 }
